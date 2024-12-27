5dc70a3f5461b23d7da4563aac9936d295b1a01d
Ozod26 <ozodfarid46@gmail.com>
2024-12-28 01:07:00 +0900

    feat: integration Header and Footer via Layoutts

---
+++ pages/property/detail..tsx
@@ -0,0 +1,10 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+
+const PropertyDetail = () => {
+  console.log("PROPERTY LIST COMPONENT - PAGES ROUTER");
+
+  return <Container>PROPERTY DETAIL</Container>;
+};
+
+export default withLayoutBasic(PropertyDetail);
